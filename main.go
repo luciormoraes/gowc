f765388d2c0cba2408cdee55b70adf8a9bbe7781
Lucio Moraes <luciomoraes@gmail.com>
2024-01-10 15:37:34 +1300

    Add .gitignore file and implement command line flag for showing lines in main.go

--- main.go
+++ main.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
-	// To read from stdin
-	if len(os.Args) < 2 {
-		fmt.Println("please pass a file name")
+	var showLines bool
+	flag.BoolVar(&showLines, "l", false, "Show lines")
+	flag.Parse()
+
+	// Get file name from command line
+	fileName := flag.CommandLine.Arg(0)
+	if flag.NArg() > 0 { // flag.NArg() returns the number of arguments remaining after flags have been processed
+		fileName = flag.Arg(0)
+	} else {
+		fmt.Println("Provide a file name")
 		os.Exit(1)
 	}
-
-	fileName := os.Args[1]
 
 	// Open file
 	file, err := os.Open(fileName)
@@ -24,6 +30,7 @@ func main() {
 	}
 	defer file.Close() // Close file when done
 
+	// Scan file
 	scanner := bufio.NewScanner(file)
 	lines, words, characters := 0, 0, 0
 
@@ -43,4 +50,8 @@ func main() {
 	}
 
 	fmt.Printf("%d %d %d %s\n", lines, words, characters, fileName)
+
+	if showLines {
+		fmt.Printf("%d %s\n", lines, fileName)
+	}
 }
